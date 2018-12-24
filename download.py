7cc99dcd88ca47ef40a8a4e0afdb4cb5572d9547
zhuxiaokai <916403797@qq.com>
2018-12-24 15:18:54 +0800

    Initial commit

---
+++ download.py
@@ -0,0 +1,7 @@
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+print(".......")
+print(mnist.test.images.shape)
+print(mnist.train.images[0,:])
